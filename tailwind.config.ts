1775620bdbeea35997a9d972ccb02f8fda972db4
Zachary Distant <zacdistant@gmail.com>
2025-06-22 20:23:05 +0100

    Folders and pages created

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,8 @@
-import type { Config } from "tailwindcss"
-
+import type { Config } from 'tailwindcss'
 const config: Config = {
-  darkMode: "class",
-  content: [
-    "./app/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./pages/**/*.{ts,tsx}",
-    "./src/**/*.{ts,tsx}"
-  ],
-  theme: {
-    extend: {},
-  },
+  darkMode: 'class',
+  content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
+  theme: { extend: {} },
   plugins: [],
 }
 export default config
