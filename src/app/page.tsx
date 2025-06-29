4c848ead4c2600f24ac0a0e2663252c7f0b24443
Zachary Distant <zacdistant@gmail.com>
2025-06-29 04:32:41 +0100

    components added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
+import CTA from "@/components/CTA";
+import Featured from "@/components/Featured";
 import HomeHero from "@/components/HomeHero";
+import Pricing from "@/components/Pricing";
+import Stats from "@/components/Stats";
+import Testimonials from "@/components/Testimonials";
 
 export default function Home() {
   return (
    <>
    <HomeHero />
+   <Stats />
+   <Featured />
+   <Testimonials />
+   <Pricing />
+   <CTA />
+   {/* <Footer /> */}
    </>
   );
 }
